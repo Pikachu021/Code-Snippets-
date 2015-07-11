6ddbfbd21f12b239970a8e7ed5a46635f8a7bf66
Pikachu021 <renatoreyesdeleon@gmail.com>
2015-07-10 21:19:10 -0600

    Upload code snippet
    This code snippet shows how to call a Server Method from script side.

---
+++ WebMethod AJAX/functions.js
@@ -0,0 +1,21 @@
+﻿jQuery(function () {
+    $('#btn1').click(function () {
+        var args = {};
+        args["ID"] = "320498232309482";
+        args["Name"] = "John Doe";
+        data = JSON.stringify(args);
+        $.ajax({
+            type: 'POST',
+            url: 'Home.aspx/testMethod',
+            data: data,
+            cache: false,
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                $('#idLBL').text(data.d.ID);
+                $('#nameLBL').text(data.d.Name);
+            }
+        });
+        return false;
+    });
+});
